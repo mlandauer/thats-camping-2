dc35ca77691ccd9d7d00e58e1e65d2125d0ba98d
Matthew Landauer <matthew@openaustralia.org>
2013-09-15 14:09:35 +1000

    Display campsites using data

--- www/js/router.js
+++ www/js/router.js
@@ -1,3 +1,9 @@
 ThatsCamping.Router.map(function () {
   this.resource('campsites', { path: '/' });
 });
+
+ThatsCamping.CampsitesRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.find('campsite');
+  }
+});
