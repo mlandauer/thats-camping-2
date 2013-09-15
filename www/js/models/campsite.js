18113821c4426e26fa5d5175fc77e868f139e101
Matthew Landauer <matthew@openaustralia.org>
2013-09-15 14:50:18 +1000

    Calculate distanceText from distance

--- www/js/models/campsite.js
+++ www/js/models/campsite.js
@@ -1,35 +1,51 @@
 ThatsCamping.Campsite = DS.Model.extend({
-  distanceText: DS.attr('string'),
+  distance: DS.attr('number'),
   bearingText: DS.attr('string'),
   name: DS.attr('string'),
-  park_name: DS.attr('string')
+  park_name: DS.attr('string'),
+
+  distanceText: function() {
+    distance = this.get("distance");
+    units = undefined
+    if(distance == null) {
+      return "";
+    }
+    if(distance > 1000) {
+      distance /= 1000;
+      units = "km";
+    }
+    else {
+      units = "m"      
+    }
+    return(distance.toFixed(0) + " " + units);
+  }.property('distance')
 });
 
 ThatsCamping.Campsite.FIXTURES = [
  {
    id: 1,
-   distanceText: '34 km',
+   distance: 34000,
    bearingText: 'NE',
    name: 'Cattai',
    park_name: 'Cattai NP'
  },
  {
    id: 2,
-   distanceText: '36 km',
+   distance: 36000,
    bearingText: 'N',
    name: 'Wheeny Creek',
    park_name: 'Wollemi NP'
  },
  {
    id: 3,
-   distanceText: '44 km',
+   distance: 44000,
    bearingText: 'N',
    name: 'Colo Meroo',
    park_name: 'Wollemi NP'
  },
  {
    id: 4,
-   distanceText: '48 km',
+   distance: 48000,
    bearingText: 'E',
    name: 'Lane Cove River',
    park_name: 'Lane Cove NP'
