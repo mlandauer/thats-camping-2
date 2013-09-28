ce7cda6f32fc85d3754eecf2ad9338ec621cf51d
Matthew Landauer <matthew@openaustralia.org>
2013-09-28 14:56:10 +1000

    Load initial data from local json file

--- www/js/router.js
+++ www/js/router.js
@@ -2,6 +2,15 @@ App.Router.map(function () {
   this.resource('campsites', { path: '/' });
 });
 
+App.ApplicationRoute = Ember.Route.extend({
+  model: function() {
+    var store = this.get('store');
+    $.getJSON("res/data/campsites.json", function(data){
+      store.pushMany('campsite', data);
+    });
+  }
+});
+
 App.CampsitesRoute = Ember.Route.extend({
   model: function () {
     return this.store.find('campsite');
