de0d2cdcdd046cbbed0dd5723c65e62298fb063e
Matthew Landauer <matthew@openaustralia.org>
2013-09-29 13:09:03 +1000

    Load data synchronously

--- www/js/router.js
+++ www/js/router.js
@@ -6,8 +6,17 @@ App.Router.map(function () {
 App.ApplicationRoute = Ember.Route.extend({
   model: function() {
     var store = this.get('store');
-    $.getJSON("res/data/campsites.json", function(data){
-      store.pushMany('campsite', data);
+    // Loading campsites data synchronously so that we know the data is loaded
+    // before something like CampsiteRoute tries to get data from it.
+    // This isn't necessary if we don't explicitly load one of the campsite detail
+    // pages directly from the url which is the case when we use it as a PhoneGap app.
+    $.ajax({
+      dataType: "json",
+      url: "res/data/campsites.json",
+      async: false,
+      success: function(data) {
+        store.pushMany('campsite', data);
+      }
     });
   }
 });
