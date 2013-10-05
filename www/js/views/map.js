df4fae18b313f340ac09e4dae1000a9eb3646b03
Matthew Landauer <matthew@openaustralia.org>
2013-10-06 10:59:09 +1100

    Add map using leaflet.js and cloudmade

---
+++ www/js/views/map.js
@@ -0,0 +1,10 @@
+App.MapView = Ember.View.extend({
+  didInsertElement: function() {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('http://{s}.tile.cloudmade.com/be7a385fc3034f60805677eadddb4130/997/256/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+      maxZoom: 18
+    }).addTo(map);
+  }
+});
