e2c880abf16b09888e98cc762308478322f553d7
Matthew Landauer <matthew@openaustralia.org>
2013-10-06 19:40:36 +1100

    Extract common code

--- www/js/views/map.js
+++ www/js/views/map.js
@@ -3,16 +3,8 @@ App.MapView = Ember.View.extend({
     return L.map('map');
   }.property(),
   didInsertElement: function() {
-    latitude = App.get("latitude");
-    longitude = App.get("longitude");
-    zoom = 13
-    if (latitude == null || longitude == null) {
-      // Show all of Australia
-      latitude = -35;
-      longitude = 133;
-      zoom = 3;
-    }
-    var map = this.get("map").setView([latitude, longitude], zoom);
+    this.didUpdateLocation();
+    var map = this.get("map");
 
     L.tileLayer('http://{s}.tile.cloudmade.com/be7a385fc3034f60805677eadddb4130/997/256/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
@@ -23,8 +15,12 @@ App.MapView = Ember.View.extend({
   didUpdateLocation: function() {
     latitude = App.get("latitude");
     longitude = App.get("longitude");
-    if (latitude != null && longitude != null) {
-      this.get("map").setView([latitude, longitude], 13);      
+    zoom = 13;
+    if (latitude == null || longitude == null) {
+      latitude = -35;
+      longitude = 133;
+      zoom = 3;
     }
+    this.get("map").setView([latitude, longitude], zoom);
   }.observes("App.latitude", "App.longitude")
 });
