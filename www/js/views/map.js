2113f60409e4e6da7c19c77a1d5ef1dba4171aba
Matthew Landauer <matthew@openaustralia.org>
2013-10-06 13:59:54 +1100

    Centre map on current location

--- www/js/views/map.js
+++ www/js/views/map.js
@@ -6,5 +6,7 @@ App.MapView = Ember.View.extend({
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
       maxZoom: 18
     }).addTo(map);
+    // Make leaflet get the current location
+    map.locate({setView: true, maxZoom: 16});
   }
 });
