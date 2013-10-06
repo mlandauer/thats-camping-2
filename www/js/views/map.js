40d9134e4e421f1740f58068304fa57c16bb005c
Matthew Landauer <matthew@openaustralia.org>
2013-10-06 19:21:44 +1100

    Nicely refocus map when location updates and start with all of australia

--- www/js/views/map.js
+++ www/js/views/map.js
@@ -1,12 +1,30 @@
 App.MapView = Ember.View.extend({
+  map: function() {
+    return L.map('map');
+  }.property(),
   didInsertElement: function() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    latitude = App.get("latitude");
+    longitude = App.get("longitude");
+    zoom = 13
+    if (latitude == null || longitude == null) {
+      // Show all of Australia
+      latitude = -35;
+      longitude = 133;
+      zoom = 3;
+    }
+    var map = this.get("map").setView([latitude, longitude], zoom);
 
     L.tileLayer('http://{s}.tile.cloudmade.com/be7a385fc3034f60805677eadddb4130/997/256/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
       maxZoom: 18
     }).addTo(map);
-    // Make leaflet get the current location
-    map.locate({setView: true, maxZoom: 16});
-  }
+  },
+
+  didUpdateLocation: function() {
+    latitude = App.get("latitude");
+    longitude = App.get("longitude");
+    if (latitude != null && longitude != null) {
+      this.get("map").setView([latitude, longitude], 13);      
+    }
+  }.observes("App.latitude", "App.longitude")
 });
