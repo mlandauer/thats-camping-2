fcb9e2fbb906aaa11871800edeb8aa98a7c07079
Matthew Landauer <matthew@openaustralia.org>
2013-10-07 15:41:51 +1100

    Make touch events feel quicker

--- www/js/application.js
+++ www/js/application.js
@@ -1,3 +1,6 @@
 window.App = Ember.Application.create();
 
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+// Make touch events on buttons & links feel quicker
+document.addEventListener("touchstart", function(){}, true)
