7cff2dee39273c133a6d211a1559f1b01d7c06d1
Matthew Landauer <matthew@openaustralia.org>
2013-10-14 14:47:01 +1100

    Not necessary (I think) on Android

--- www/js/application.js
+++ www/js/application.js
@@ -1,6 +1,3 @@
 window.App = Ember.Application.create();
 
 App.ApplicationAdapter = DS.RESTAdapter.extend();
-
-// Make touch events on buttons & links feel quicker
-document.addEventListener("touchstart", function(){}, true)
