2b92be5374bff9481e9588d404baa10b93d19438
Matthew Landauer <matthew@openaustralia.org>
2013-10-16 11:52:30 +1100

    Make the RESTAdapter work on the phone in Phonegap

--- www/js/application.js
+++ www/js/application.js
@@ -1,3 +1,8 @@
 window.App = Ember.Application.create();
 
-App.ApplicationAdapter = DS.RESTAdapter.extend();
+App.ApplicationAdapter = DS.RESTAdapter.extend({
+  namespace: function() {
+    // Use the current directory as the namespace
+    return window.location.pathname.split("/").slice(1,-1).join("/");
+  }.property()
+});
